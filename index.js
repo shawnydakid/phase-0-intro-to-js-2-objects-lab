e7fa469a03400bb3eaf0e7264c38496190e3c3ba
Lashawn Butler <lashawnbutler50@gmail.com>
2023-11-07 18:57:29 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name: "Shawn",
+    streetAddress: "1920 Loring PL S"
+}
+function  updateEmployeeWithKeyAndValue(employee, key, value) {
+    const copy = {... employee}
+    copy[key] = value
+    return copy 
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key, value) {
+    const copy = {... employee}
+    delete copy[key]
+    return copy
+
+   
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key]
+    return employee
+}
